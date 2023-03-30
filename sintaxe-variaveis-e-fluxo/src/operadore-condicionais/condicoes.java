b5482adf48aab95cc9f8907a32536ea65a206577
cauan <cauanbarros14@gmail.com>
2023-03-30 12:20:39 -0300

    boolean

--- sintaxe-variaveis-e-fluxo/src/operadore-condicionais/condicoes.java
+++ sintaxe-variaveis-e-fluxo/src/operadore-condicionais/condicoes.java
@@ -1,11 +1,12 @@
 class programa {
    public static void main(String[] args) {
     System.out.println("testando condicionais");
-    int idade = 17;
-    if (idade >= 18) {
+    int idade = 7;
+    int quantidadePessoas = 2;
+    if (idade >= 18 || quantidadePessoas >= 2) {
         System.out.println("maior de idade");
     } else {
         System.out.println("voce nao pode entrar");
+        }
     }
    } 
-}
