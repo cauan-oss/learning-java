4be4eeb98227825b69afe43ac7184749f2a8812a
cauan <cauanbarros14@gmail.com>
2023-03-29 23:16:28 -0300

    regras de double e divisao

---
+++ sintaxe-variaveis-e-fluxo/src/TipoDouble.java
@@ -0,0 +1,13 @@
+// variaveis do tipo double usamos quando temos numero com ponto;
+
+ class testapontoflutuante {
+    public static void main(String[] args) {
+       double salario = 1250.70;
+        System.out.println("meu salario e " + salario);
+
+        // divisao 
+//      regra:divisao usando o double o numero da esquerda tem que ter ponto;
+        double tentativa = 5.0 / 2;
+        System.out.println(tentativa);
+    }
+}
---
+++ sintaxe-variaveis-e-fluxo/src/testaVariaveis.java
@@ -0,0 +1,26 @@
+// variaveis e concatenacao;
+public class testaVariaveis {
+  public  static void main(String[] args) {
+    System.out.println("ola novo teste");
+    int age;
+    age = 37;
+    System.out.println(age);
+    age  = 30 + 10;
+    System.out.println(age);
+    age = 19;
+    System.out.println("a idade do cauan e " + age);
+  };
+};
+
+ class brincando {
+  public static void main(String[] args) {
+    System.out.println("brincando com java");
+    int ageCauan;
+    ageCauan = 19;
+    int ageClaudio;
+    ageClaudio = 46;
+    if (ageClaudio > ageCauan){
+        System.out.println("o pai e sempre mais velho que o filho");
+    }
+  }
+};
