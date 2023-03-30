7f6a3934a13e038114b1709860be230aa344cb64
cauan <cauanbarros14@gmail.com>
2023-03-30 12:07:46 -0300

    condicinais ok

---
+++ sintaxe-variaveis-e-fluxo/src/operadore-condicionais/condicoes.java
@@ -0,0 +1,11 @@
+class programa {
+   public static void main(String[] args) {
+    System.out.println("testando condicionais");
+    int idade = 17;
+    if (idade >= 18) {
+        System.out.println("maior de idade");
+    } else {
+        System.out.println("voce nao pode entrar");
+    }
+   } 
+}
