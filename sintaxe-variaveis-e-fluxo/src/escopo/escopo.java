609271a735dd64e16593781cf8df0a11365ae867
cauan <cauanbarros14@gmail.com>
2023-03-30 12:35:32 -0300

    escopo de variavel

---
+++ sintaxe-variaveis-e-fluxo/src/escopo/escopo.java
@@ -0,0 +1,13 @@
+class escopo {
+    public static void main(String[] args) {
+        System.out.println("escopo de variavel");
+        int quantidadePessoas = 2;
+        boolean acompanhado = true;
+        if (quantidadePessoas >= 2){
+            acompanhado = true
+        }
+        else {
+            acompanhado = false;
+        };
+    }
+}
