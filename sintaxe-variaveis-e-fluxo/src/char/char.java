0c8272f52e218dd1b82daede27b0e72f13a75539
cauan <cauanbarros14@gmail.com>
2023-03-30 11:37:57 -0300

    char e caracter

---
+++ sintaxe-variaveis-e-fluxo/src/char/char.java
@@ -0,0 +1,11 @@
+class caracteres {
+    public static void main(String[] args) {
+        // para escrever uma letra usa o char;
+        char letra = 'a';
+        System.out.println(letra + 1);
+
+        // para escrever uma frase usa o string;
+        String palavra = "estudando java";
+        System.out.println(palavra);
+    }
+}
