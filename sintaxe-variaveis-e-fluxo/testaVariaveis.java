1fba71ed6629e644dbb752c0828e8ad6829b738c
cauan <cauanbarros14@gmail.com>
2023-03-29 22:39:17 -0300

    primeiras variaveis no java e concatenacao

---
+++ sintaxe-variaveis-e-fluxo/testaVariaveis.java
@@ -0,0 +1,13 @@
+// variaveis e concatenacao;
+public class testaVariaveis {
+  public  static void main(String[] args) {
+    System.out.println("ola novo teste");
+    int age;
+    age = 37;
+    System.out.println(age);
+    age  = 30 + 10;
+    System.out.println(age);
+    age = 19;
+    System.out.println("a idade do cauan e " + age);
+  };
+};
