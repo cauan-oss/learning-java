d59d761b73a0b54d1dffac87045cb911115d9b2b
cauan <cauanbarros14@gmail.com>
2023-03-30 12:26:55 -0300

    exec bool

--- sintaxe-variaveis-e-fluxo/src/operadore-condicionais/condicoes.java
+++ sintaxe-variaveis-e-fluxo/src/operadore-condicionais/condicoes.java
@@ -2,9 +2,11 @@ class programa {
    public static void main(String[] args) {
     System.out.println("testando condicionais");
     int idade = 7;
-    int quantidadePessoas = 2;
-    if (idade >= 18 || quantidadePessoas >= 2) {
-        System.out.println("maior de idade");
+
+    // variavel boleano so recebe true ou false;
+    boolean acompanhado = true;
+    if (idade >= 18 && acompanhado) {
+        System.out.println("seja bem vindo");
     } else {
         System.out.println("voce nao pode entrar");
         }
