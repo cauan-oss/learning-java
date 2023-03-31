25255d503be0ac42b270293c28b7afb388772ca5
cauan <cauanbarros14@gmail.com>
2023-03-30 23:37:27 -0300

    laco for

---
+++ sintaxe-variaveis-e-fluxo/src/lacoFor/laco.java
@@ -0,0 +1,8 @@
+class testaFor {
+    public static void main(String[] args) {
+        System.out.println("testando o for");
+        for(int contador=0; contador <=10; contador += 1) {
+            System.out.println(contador);
+        }
+    }
+}
