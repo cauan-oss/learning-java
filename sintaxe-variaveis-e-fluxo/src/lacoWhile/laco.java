4fd840777d5c9f7d092741e6aac88a65e6ce1676
cauan <cauanbarros14@gmail.com>
2023-03-30 23:33:02 -0300

    while

---
+++ sintaxe-variaveis-e-fluxo/src/lacoWhile/laco.java
@@ -0,0 +1,11 @@
+class testaWhile {
+  public static void main(String[] args) {
+    System.out.println("testa while");
+
+    int contador = 0;
+    while(contador <= 10){
+        contador += 1;
+        System.out.println(contador);
+    }
+  }
+};
